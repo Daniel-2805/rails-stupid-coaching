383115c9fcb67decb10eb9b5c192383ca4e7e8ab
daniel vega <daniel7491@gmail.com>
2021-07-27 10:08:11 -0400

    Segundo intento del ejercicio 2

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,6 +1,13 @@
 class QuestionsController < ApplicationController
   def ask
   end
-  def answer 
+  def answer
+    @question = params[:question]
+    @answers = "I don't care, get dressed and go to work!"
+    if @question == "I am going to work"
+      @answers = "Great!"
+    elsif @question.end_with?("?")
+      @answers = "Silly question, get dressed and go to work!"
+    end
   end
 end
